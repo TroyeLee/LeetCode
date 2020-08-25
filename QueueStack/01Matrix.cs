7f28e77b71729cf9eb47ff6d4b4a58ccf7fe9f85
TroyeLee <1390546753@qq.com>
2020-08-25 17:08:45 +0800

    Thrid

---
+++ QueueStack/01Matrix.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QueueStack
+{
+    public class _01Matrix
+    {
+        /// <summary>
+        /// 01 矩阵
+        /// 给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
+        /// 两个相邻元素间的距离为 1 。
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public int[][] UpdateMatrix(int[][] matrix)
+        {
+            //利用广度优先的思想，对每个元素进行广度优先遍历，找到最近的0
+            if(matrix.Length == 0)
+            {
+                return matrix;
+            }
+
+            int[][] result = matrix;
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i][j] = Distance_BFS(i, j, matrix);
+                }
+            }
+
+            return result;
+        }
+        private int Distance_BFS(int i , int j , int[][]matrix)
+        {
+            if (matrix[i][j] == 0)
+                return 0;
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            int[][] direction = new int[][] { 
+                new int[]{ 1,0},
+                new int[]{ -1,0},
+                new int[]{ 0,1},
+                new int[]{ 0,-1}
+            };
+            Queue<int> queue = new Queue<int>();
+            bool[,] visited = new bool[rows, cols];
+            int result = 0;
+            queue.Enqueue(i*cols+j);
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                for (int s = 0; s < size; s++)
+                {
+                    int cur = queue.Dequeue();
+                    int x = cur / cols;
+                    int y = cur % cols;
+                    visited[x, y] = true;
+                    if (matrix[x][y] == 0)
+                    {
+                        return result;
+                    }
+                    for (int m = 0; m < 4; m++)
+                    {
+                        int newX = x + direction[m][0];
+                        int newY = y + direction[m][1];
+                        if (InMatrix(newX, newY, matrix) && !visited[newX, newY])
+                        {
+                            queue.Enqueue(newX * cols + newY);
+                            visited[newX, newY] = true;
+                        }
+                    }
+                }
+                
+                result++;
+            }
+            return result;
+        }
+        private bool InMatrix(int x , int y , int[][] matrix)
+        {
+            return x >= 0 && x < matrix.Length && y >= 0 && y < matrix[0].Length;
+        }
+    }
+}
---
+++ QueueStack/KeysAndRooms.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QueueStack
+{
+    public class KeysAndRooms
+    {
+        /// <summary>
+        /// 钥匙和房间
+        ///   有 N 个房间，开始时你位于 0 号房间。每个房间有不同的号码：0，1，2，...，N-1，并且房间里可能有一些钥匙能使你进入下一个房间。
+        ///   在形式上，对于每个房间 i 都有一个钥匙列表 rooms[i]，每个钥匙 rooms[i][j] 由[0, 1，...，N - 1] 中的一个整数表示，其中 N = rooms.length。 钥匙 rooms[i][j] = v 可以打开编号为 v 的房间。
+        ///   最初，除 0 号房间外的其余所有房间都被锁住。
+        ///   你可以自由地在房间之间来回走动。
+        ///   如果能进入每个房间返回 true，否则返回 false。
+        ///     输入：[[1,3],[3,0,1],[2],[0]]
+        ///     输出：false
+        ///     解释：我们不能进入 2 号房间。
+        /// </summary>
+        /// <param name="rooms"></param>
+        /// <returns></returns>
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            if (rooms.Count == 0)
+            {
+                return false;
+            }
+            HashSet<int> visited = new HashSet<int>();
+
+            Queue<int> queue = new Queue<int>();
+
+            GetKeys(rooms, queue, 0, visited);
+            visited.Add(0);
+            while (queue.Count > 0)
+            {
+                int room = queue.Dequeue();
+                GetKeys(rooms,queue,room,visited);
+            }
+
+            return visited.Count == rooms.Count;
+        }
+
+        private void GetKeys(IList<IList<int>> rooms , Queue<int> queue , int room , HashSet<int> visited)
+        {
+            for (int i = 0; i < rooms[room].Count; i++)
+            {
+                if (!visited.Contains(rooms[room][i]))
+                {
+                    queue.Enqueue(rooms[room][i]);
+                    visited.Add(rooms[room][i]);
+                }
+            }
+        }
+    }
+}
