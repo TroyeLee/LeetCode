823d910764296c18a543c666827ab126eaf5e6cd
TroyeLee <1390546753@qq.com>
2020-09-18 18:45:19 +0800

    0918

---
+++ HashTable/ContainsDuplicateII.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    public class ContainsDuplicateII
+    {
+        /// <summary>
+        /// 存在重复元素 II
+        /// 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            if (nums.Length == 0 || nums == null)
+            {
+                return false;
+            }
+            Dictionary<int, HashSet<int>> map = new Dictionary<int, HashSet<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int differ = k + i;
+                if (map.ContainsKey(nums[i]))
+                {
+                    if (map[nums[i]].Any(u => u >= i))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        map[nums[i]].Add(differ);
+                    }
+                }
+                else
+                {
+                    map.Add(nums[i], new HashSet<int>() { differ });
+                }
+
+            }
+
+            return false;
+        }
+
+        public bool ContainsNearbyDuplicate02(int[] nums, int k)
+        {
+            if (nums.Length == 0 || nums == null)
+            {
+                return false;
+            }
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    int index = map[nums[i]];
+                    int differ = Math.Abs(i - index);
+                    if(differ <= k)
+                    {
+                        return true;
+                    }
+                    map[nums[i]] = i;
+                }
+                else
+                {
+                    map.Add(nums[i], i);
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ HashTable/GroupAnagramsCS.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    public class GroupAnagramsCS
+    {
+        /// <summary>
+        /// 字母异位词分组
+        /// 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+        /// 示例:
+        ///     输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+        ///     输出:
+        ///     [
+        ///     ["ate","eat","tea"],
+        ///     ["nat","tan"],
+        ///     ["bat"]
+        ///     ]
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            
+            IList<IList<string>> result = new List<IList<string>>();
+            if(strs.Length==0|| strs == null)
+            {
+                return result;
+            }
+            Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                string sortString = SortString(strs[i]);
+                if (map.ContainsKey(sortString))
+                {
+                    map[sortString].Add(strs[i]);
+                }
+                else
+                {
+                    map.Add(sortString, new List<string>() { strs[i]});
+                }
+            }
+            foreach (var item in map)
+            {
+                result.Add(item.Value);
+            }
+
+            return result;
+         }
+
+        private string SortString(string str)
+        {
+            string result = "";
+            char[] chars = str.ToCharArray();
+            int min;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                min = i;
+                for (int j = i+1; j < chars.Length ; j++)
+                {
+                    min = chars[j] < chars[min] ? j : min;
+                }
+                char temp = chars[i];
+                chars[i] = chars[min];
+                chars[min] = temp;
+                result += chars[i];
+            }
+
+            return result;
+
+        }
+
+
+        /// <summary>
+        /// 对26个字母分别赋予对应的质数值，因为不同的质数和必定为不同的数字结果，所以可以用来作为map的key值
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public IList<IList<string>> GroupAnagrams02(string[] strs)
+        {
+            IList<IList<string>> result = new List<IList<string>>();
+            if (strs.Length == 0 || strs == null)
+            {
+                return result;
+            }
+            Dictionary<int, IList<string>> map = new Dictionary<int, IList<string>>();
+            foreach (var item in strs)
+            {
+                int key = BuildKey(item);
+                if (map.ContainsKey(key))
+                {
+                    map[key].Add(item);
+                }
+                else
+                {
+                    map.Add(key, new List<string>() { item });
+                }
+            }
+            foreach (var item in map)
+            {
+                result.Add(item.Value);
+            }
+
+            return result.Reverse().ToArray() ;
+        }
+
+        private int BuildKey(string str)
+        {
+            int result = 1;
+
+            int[] a = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 };
+
+            foreach (var item in str)
+            {
+                result *= a[item - 'a'];
+            }
+
+            return result;
+        }
+    }
+}
