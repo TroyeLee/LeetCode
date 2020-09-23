f95e2e69f1a308097321df54061fdfad553dff0e
TroyeLee <1390546753@qq.com>
2020-09-23 17:41:21 +0800

    0923

---
+++ HashTable/JewelsAndStones.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 宝石与石头
+    /// 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。
+    /// S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+    /// J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+    /// </summary>
+    public class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int result = 0;
+            IDictionary<char, int> map = new Dictionary<char, int>();
+            if (J.Length == 0 || S.Length == 0)
+                return result;
+            foreach (var item in S)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+            foreach (var item in J)
+            {
+                if (map.ContainsKey(item))
+                {
+                    result += map[item];
+                }
+            }
+
+            return result;
+        }
+
+        public int NumJewelsInStones02(string J, string S)
+        {
+            int result = 0;
+            HashSet<char> Set = new HashSet<char>();
+            if (J.Length == 0 || S.Length == 0)
+                return result;
+            foreach (var item in J)
+            {
+                if(!Set.Contains(item))
+                    Set.Add(item);
+            }
+            foreach (var item in S)
+            {
+                if (Set.Contains(item))
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HashTable/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 无重复字符的最长子串
+    /// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+    /// </summary>
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            if (s.Length == 0 || s == null)
+            {
+                return 0;
+            }
+            HashSet<char> set = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (set.Contains(s[j]))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        set.Add(s[j]);
+                    }
+                }
+
+                result = result > set.Count() ? result : set.Count();
+                set.Clear();
+            }
+
+            return result;
+        }
+
+        public int LengthOfLongestSubstring02(string s)
+        {
+            int result = 0;
+            if (s.Length == 0 || s == null)
+            {
+                return 0;
+            }
+            Queue<char> set = new Queue<char>();
+            for (int i = 0; i < s.Length; )
+            {
+                if (set.Contains(s[i]))
+                {
+                    result = Math.Max(result, set.Count());
+                    set.Dequeue();
+                }
+                else
+                {
+                    set.Enqueue(s[i]);
+                    i++;
+                }
+            }
+            result = Math.Max(result, set.Count());
+            return result;
+        }
+    }
+}
