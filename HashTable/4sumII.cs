952dde71c616046e9e5773cba4fc0dd24293537b
TroyeLee <1390546753@qq.com>
2020-09-24 17:49:11 +0800

    0924

---
+++ HashTable/4sumII.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    public class _4sumII
+    {
+        public int FourSumCount(int[] A, int[] B, int[] C, int[] D)
+        {
+            int result = 0;
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            foreach (var itemA in A)
+            {
+                foreach (var itemB in B)
+                {
+                    int nums = itemA + itemB;
+                    if (map.ContainsKey(nums))
+                    {
+                        map[nums]++;
+                    }
+                    else
+                    {
+                        map.Add(nums, 1);
+                    }
+                }
+            }
+
+            foreach (var itemC in C)
+            {
+                foreach (var itemD in D)
+                {
+                    int nums = 0-(itemC + itemD);
+                    if (map.ContainsKey(nums))
+                    {
+                        result += map[nums];
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ HashTable/TopKFrequentElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    public class TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            HashSet<int> result = new HashSet<int>();
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                    if(map[item]>k && !result.Contains(item))
+                    {
+                        result.Add(item);
+                    }
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+            
+
+            return result.ToArray();
+        }
+    }
+}
