cbb4e6a0f18c4def04f6d9f71c4d2252f5122708
TroyeLee <1390546753@qq.com>
2020-09-25 18:30:44 +0800

    0925

---
+++ HashTable/RandomizedSet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 常数时间插入、删除和获取随机元素
+    /// 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+    /// insert(val)：当元素 val 不存在时，向集合中插入该项。
+    /// remove(val)：元素 val 存在时，从集合中移除该项。
+    /// getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+    /// </summary>
+    public class RandomizedSet
+    {
+
+        /** Initialize your data structure here. */
+        IDictionary<int, int> index_value;
+        IDictionary<int, int> value_index;
+        int size;
+        public RandomizedSet()
+        {
+            index_value = new Dictionary<int, int>();
+            value_index = new Dictionary<int, int>();
+            size = 0;
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public bool Insert(int val)
+        {
+            if (value_index.ContainsKey(val))
+            {
+                return false;
+            }
+            index_value.Add(size, val);
+            value_index.Add(val, size);
+            size++;
+            return true;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public bool Remove(int val)
+        {
+            if (!value_index.ContainsKey(val))
+            {
+                return false;
+            }
+            if (size == 1) {
+                value_index.Remove(val);
+                index_value.Remove(0);
+            }
+            int index = value_index[val];
+            value_index.Remove(val);
+
+            int last_val = index_value[size-1];
+            value_index[last_val] = index;            // 将最后一位替换到remove的位置
+
+            index_value[index] = index_value[size-1]; // 将最后一位替换到remove的位置
+            index_value.Remove(size-1);               // remove最后一位
+            size--;
+            return true;
+        }
+
+        /** Get a random element from the set. */
+        public int GetRandom()
+        {
+            int index = new Random().Next(0,size);
+            return index_value[index];
+        }
+    }
+}
--- HashTable/TopKFrequentElements.cs
+++ HashTable/TopKFrequentElements.cs
@@ -6,28 +6,59 @@ using System.Threading.Tasks;
 
 namespace LeetCode.HashTable
 {
+    /// <summary>
+    /// 前 K 个高频元素
+    /// 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+    /// 示例 1:
+    /// 输入: nums = [1,1,1,2,2,3], k = 2
+    /// 输出: [1,2]
+    /// </summary>
     public class TopKFrequentElements
     {
+        
         public int[] TopKFrequent(int[] nums, int k)
         {
-            HashSet<int> result = new HashSet<int>();
+            //先获取各个数字出现频率，然后利用列表数组对其进行排序，以出现频率为索引，出现相同次数的放到同一个list中，最后对这个数组反序输出。
+            IList<int> result = new List<int>();
             IDictionary<int, int> map = new Dictionary<int, int>();
             foreach (var item in nums)
             {
                 if (map.ContainsKey(item))
                 {
                     map[item]++;
-                    if(map[item]>k && !result.Contains(item))
-                    {
-                        result.Add(item);
-                    }
                 }
                 else
                 {
                     map.Add(item, 1);
                 }
             }
-            
+            IList<int>[] temp = new List<int>[nums.Length+1];
+            foreach (var item in map)
+            {
+                if(temp[item.Value]==null)
+                    temp[item.Value] = new List<int>();
+                temp[item.Value].Add(item.Key);
+            }
+            int count = 0;
+            for (int i = temp.Length-1; i >= 0; i--)
+            {
+                if (temp[i] == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    foreach (var item in temp[i])
+                    {
+                        result.Add(item);
+                        count++;
+                        if (count == k)
+                        {
+                            return result.ToArray();
+                        }
+                    }
+                }
+            }
 
             return result.ToArray();
         }
