02c7f7df8d8c7bae81d8d381a52d4c3ff06b61ad
TroyeLee <1390546753@qq.com>
2020-10-10 15:17:09 +0800

    1010-填充右指针

--- DataStructureBinaryTree/PopulatingNextRightPointersInEachNode.cs
+++ DataStructureBinaryTree/PopulatingNextRightPointersInEachNode.cs
@@ -21,7 +21,33 @@ namespace LeetCode.DataStructureBinaryTree
     {
         public Node Connect(Node root)
         {
-            
+            if (root == null)
+            {
+                return null;
+            }
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    Node node = queue.Dequeue();
+                    node.next = i == size - 1 ? null : queue.Peek();
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+
+            return root;
         }
     }
 }
